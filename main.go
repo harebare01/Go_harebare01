2c8cc54fc1796b7b61305b866e74bf0054599d7c
harebare01 <harebare01@gmail.com>
2020-05-09 11:41:48 +0000

    index.html css bulma

--- main.go
+++ main.go
@@ -9,19 +9,20 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("(Content-Type", "text/html; charset=utf8")
 
   str := `
-  <!DOCTYPE html>
-  <html>
-    <meta charset='UTF-8'>
-    <body>
-       <h1>Hello Docker <font color=‘green’>二階堂春雄</font>   on CentOS7 .</h1>
-    </body>
-  </html>`
-
+<!DOCTYPE html>
+<html lang="ja">
+<head><meta charset="utf-8"><title>Dockerデモ</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+  <body>
+    <div class="has-text-centered">
+      <dev class="is-size-1">二階堂 with CI/CI</dev>
+    </dev>
+  </body>
+</html>`
   w.Write([]byte(str))
 }
 
 func main() {
   http.HandleFunc("/", rootHandler)
   log.Fatal(http.ListenAndServe(":80", nil))
-}
 
