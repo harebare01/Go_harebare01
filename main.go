ac7da4669f9d6bd31064aafc82ff29e88d4ff5fa
harebare01 <harebare01@gmail.com>
2020-05-18 04:52:04 +0000

    1'st commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+import (
+  "net/http"
+  "log"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(200)
+  w.Header().Set("(Content-Type", "text/html; charset=utf8")
+
+  str := `
+<!DOCTYPE html>
+<html lang="ja">
+<head><meta charset="utf-8">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+<title>Dockerデモ</title>
+  <body>
+    <div class="has-text-centered">
+      <dev class="is-size-1">二階堂春雄 with CI/CD</dev>
+    </dev>
+  </body>
+</html>`
+  w.Write([]byte(str))
+}
+
+func main() {
+  http.HandleFunc("/", rootHandler)
+  log.Fatal(http.ListenAndServe(":80", nil))
+}
